abd2d7200bde310303a985aa3e326a49c37ad4dd
Nathan <nbarton915@gmail.com>
2022-02-17 10:53:35 -0700

    feat: add commandArgs option

--- optilogic/create_job.py
+++ optilogic/create_job.py
@@ -2,23 +2,29 @@ import argparse
 import json
 import requests
 
-parser = argparse.ArgumentParser(description='Create a new Optilogic Job')
-parser.add_argument('--workspace', help='Optilogic Workspace Name')
-parser.add_argument('--directoryPath', help='Optilogic Path to Directory')
-parser.add_argument('--filename', help='Optilogic Filename')
-parser.add_argument('--apiKey', help='Optilogic Token ')
+def create_job(args):
+    url = f'https://api.optilogic.app/v0/{args.workspace}/job?directoryPath={args.directoryPath}&filename={args.filename}'
+    if args.commandArgs:
+        url += f'&commandArgs={args.commandArgs}'
+    headers = {
+        'X-API-KEY': f'{args.apiKey}'
+        }
 
-args = parser.parse_args()
+    response = requests.request('POST', url, headers=headers)
+    job_object = json.loads(response.text)
+    try:
+        job_key = job_object['jobKey']
+        print(job_key)
+    except Exception as e:
+        print(f'There was an error with getting the jobKey\n\nResponse: {job_object}')
 
-url = f'https://api.optilogic.app/v0/{args.workspace}/job?directoryPath={args.directoryPath}&filename={args.filename}'
-headers = {
-	'X-API-KEY': f'{args.apiKey}'
-	}
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create a new Optilogic Job')
+    parser.add_argument('--workspace', help='Optilogic Workspace Name')
+    parser.add_argument('--directoryPath', help='Optilogic Path to Directory')
+    parser.add_argument('--filename', help='Optilogic Filename')
+    parser.add_argument('--apiKey', help='Optilogic Token ')
+    parser.add_argument('--commandArgs', help='Arg to pass to python')
 
-response = requests.request('POST', url, headers=headers)
-job_object = json.loads(response.text)
-try:
-    job_key = job_object['jobKey']
-    print(job_key)
-except Exception as e:
-    print(f'There was an error with getting the jobKey\n\nResponse: {job_object}')
+    args = parser.parse_args()
+    create_job(args)
