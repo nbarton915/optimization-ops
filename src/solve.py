1b8037fb068d1415975d566f65f0805943f75373
Nathan <nbarton915@gmail.com>
2022-02-18 11:25:29 -0700

    add debugging info

--- src/solve.py
+++ src/solve.py
@@ -24,6 +24,7 @@ import time
 minutes = args.timetest*60
 checkpoints = minutes / 10 #report on 10 second intervals
 for c in range(0, checkpoints):
+    print(f'{c} seconds have passed')
     time.sleep(10)
 
 if args.scenario:
