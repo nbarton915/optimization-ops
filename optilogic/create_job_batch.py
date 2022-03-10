391910ebafcfbd9837017f8703b2fc43214fb8f4
Nathan <nbarton915@gmail.com>
2022-03-10 08:41:35 -0700

    update data payload and add support for app key

--- optilogic/create_job_batch.py
+++ optilogic/create_job_batch.py
@@ -4,23 +4,30 @@ import requests
 
 def create_job_batch(args):
     url = f'https://api.optilogic.app/v0/{args.workspace}/job/batch?'
-    if hasattr(args, 'searchForMatches'):
+    if args.searchForMatches:
         url += f'&searchForMatches={args.searchForMatches}'
-    if hasattr(args, 'jobify'):
+    if args.jobify:
         url += f'&jobify={args.jobify}'
     if hasattr(args, 'timeout') and args.timeout:
         url += f'&timeout={args.timeout}'
+    if args.verboseOutput:
+        url += f'&verboseOutput={args.verboseOutput}'
     if args.jobTags:
         url += f'&tags={args.jobTags}'
     if args.d:
         url = url.replace('api.', 'dev.api.')
 
-    headers = {
-        'X-API-KEY': f'{args.apiKey}'
+    if hasattr(args, 'appKey') and args.appKey:
+        headers = {
+        'X-APP-KEY': f'{args.appKey}'
         }
+    else:
+        headers = {
+            'X-API-KEY': f'{args.apiKey}'
+            }
 
     data = {
-        "batchList": [
+        "batchItems": [
             ["src/*python"],
             ["optimization-ops/src"],
             ["My Models/optimization-ops/src/"],
@@ -31,7 +38,10 @@ def create_job_batch(args):
             ]
     }
 
-    response = requests.request('POST', url, headers=headers, data=data)
+    response = requests.request('POST', url, headers=headers, json=data)
+    print(response.request.url)
+    print(response.request.headers)
+    print(response.request.body)
     job_object = json.loads(response.text)
     try:
         job_key = job_object['jobKey']
@@ -43,8 +53,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a new Optilogic Job')
     parser.add_argument('--workspace', help='Optilogic Workspace Name')
     parser.add_argument('--apiKey', help='Optilogic Token ')
+    parser.add_argument('--appKey', help='Optilogic App Key ')
     parser.add_argument('--searchForMatches', help='search path for matches')
     parser.add_argument('--jobify', help='create a unique job for each module')
+    parser.add_argument('--verboseOutput', help='controls if job log output is verbose')
     parser.add_argument('--jobTags', help='Tags to add to job')
     parser.add_argument('--timeout', help='Max time for job to run')
     parser.add_argument('-d', action='store_true')
